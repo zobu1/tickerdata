c527dbc5504665922ba120bc81f15bd579ac683b
zobu1 <93216441+zobu1@users.noreply.github.com>
2023-05-03 10:13:50 -0400

    Create fetchdata.py

---
+++ fetchdata.py
@@ -0,0 +1,15 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+
+stock = input("Enter Ticker \n")
+stock = stock.upper()
+
+print("Format: XXXX-XX-XX")
+start_date = input("Start Date \n")
+end_date = input ("End Data \n")
+period_input = input ("Period \n")
+
+data = yf.download(stock , start = start_date , end = end_date , period = period_input)
+data.to_csv(stock + ".csv")
+
